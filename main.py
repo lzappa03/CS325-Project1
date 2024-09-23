61478a4e2acd34b878053d910dcd6e4c3146ec08
Lorenzzi Zappa <lzappa@siue.edu>
2024-09-22 22:06:21 -0500

    read prompts file into program. Put prompts into a list

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import ollama
+#import ollama
 
-response = ollama.generate(model='phi3', prompt='create a short story about two birds that fall in love please')
-print(response)
+try:
+    with open('prompts.txt', 'r', encoding="utf8") as prompts: #opens and reads textfile with prompts
+        prompt_list = [line.rstrip('\n') for line in prompts] #puts prompts into list
+        print(prompt_list)
+except FileNotFoundError:
+    print("file not found")
+
+
+
+#generates a response with given prompt denoted as 'prompt'
+#response = ollama.generate(model='phi3', prompt='create a short story about two birds that fall in love please')
+#print(response)
