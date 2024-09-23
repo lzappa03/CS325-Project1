77fed21a67206e0a7eaf006be7018db1f23cd6c1
Lorenzzi Zappa <lzappa@siue.edu>
2024-09-22 22:29:01 -0500

    got prompts through phi3 and printed out their responses

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-#import ollama
+import ollama
 
 try:
     with open('prompts.txt', 'r', encoding="utf8") as prompts: #opens and reads textfile with prompts
         prompt_list = [line.rstrip('\n') for line in prompts] #puts prompts into list
-        print(prompt_list)
+        #print(prompt_list)
 except FileNotFoundError:
     print("file not found")
 
 
+for p in prompt_list:
+    response = ollama.generate(model='phi3', prompt=p) #generates a response with given prompt denoted as 'p'
+    print(p,"\n",response['response'],"\n\n") #prints prompt and response
 
-#generates a response with given prompt denoted as 'prompt'
-#response = ollama.generate(model='phi3', prompt='create a short story about two birds that fall in love please')
-#print(response)
+
+
